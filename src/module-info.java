47ff6cb523f60b8d1ce6457c34b61d7ac54342b7
Diego <dieferramos@gmail.com>
2026-05-31 12:23:31 -0500

    Primer commit semana 2

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Practica_campo02 {
+}
---
+++ src/practica/Principal.java
@@ -0,0 +1,11 @@
+package practica;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        System.out.println("Practica de Git y GitHub - Semana 2");
+
+    }
+
+}
